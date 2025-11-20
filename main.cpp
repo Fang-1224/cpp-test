705951e1a6f3c326a3a092314dc0b8dfaff0e942
Fang-1224 <s1120351@dmhs.kh.edu.tw>
2025-11-20 14:22:34 +0800

    作業9

--- main.cpp
+++ main.cpp
@@ -1,39 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int accumulate(int n);
+
 int main() {
-    int stack[5];  // 宣告堆疊空間
-    int top = -1;  // 記錄堆疊頂端位置
-
-    // 推入三個元素
-    top++;
-    stack[top] = 10;
-    top++;
-    stack[top] = 20;
-    top++;
-    stack[top] = 30;
-
-    // 輸出目前堆疊內容
-    cout << "目前堆疊內容: " << endl;
-    for (int i = 0; i <= top; i++) {
-        cout << stack[i] << " ";
-    }
-    cout << endl << endl;
-
-    // 彈出一個元素
-    if (top >= 0) {
-        cout << "彈出元素: " << stack[top] << endl;
-        top--;
-    } else {
-        cout << "堆疊是空的，不能彈出!" << endl;
-    }
-
-    // 再次輸出堆疊內容
-    cout << "彈出後堆疊內容: " << endl;
-    for (int i = 0; i <= top; i++) {
-        cout << stack[i] << " ";
-    }
-    cout << endl;
-
+    int number = 100;
+    cout << "從1加到" << number << "總合為: " << accumulate(number) << endl;
     return 0;
+}
+
+int accumulate(int n) {
+    if (n == 1) {
+        return 1;  // Base case: sum of 1 is just 1
+    }
+    return n + accumulate(n - 1);  // Recursively accumulate the sum
 }
